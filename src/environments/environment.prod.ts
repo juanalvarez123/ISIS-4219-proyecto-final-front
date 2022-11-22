ace6580182cc92f2902e53c589e0271e460e4f3c
Juan Sebastian Alvarez Eraso <js.alvareze@uniandes.edu.co>
2022-11-21 23:17:17 -0500

    Se revierte cambio para permitir llamados HTTP sin seguridad y se cambia la URL de prod a Heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-44-199-33-99.compute-1.amazonaws.com:5000'
+  apiUrl: 'https://isis-4219-proyecto-final-api.herokuapp.com'
 };
