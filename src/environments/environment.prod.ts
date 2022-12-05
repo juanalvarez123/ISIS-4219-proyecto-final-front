eda88ef9befcf43353a9d31a0a54468e6a1252d4
Juan Sebastian Alvarez Eraso <js.alvareze@uniandes.edu.co>
2022-12-04 19:57:01 -0500

    Se coloca de nuevo la URL correcta de AWS en producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  //apiUrl: 'http://ec2-44-199-33-99.compute-1.amazonaws.com:5000'
-  apiUrl: 'http://localhost:5000'
+  apiUrl: 'http://ec2-44-199-33-99.compute-1.amazonaws.com:5000'
 };
