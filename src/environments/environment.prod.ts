1ddd0bf19a5f78f0003a9a00b226a5d979b44d9c
Juan Sebastian Alvarez Eraso <js.alvareze@uniandes.edu.co>
2022-12-06 17:10:15 -0500

    Fix URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-44-199-33-99.compute-1.amazonaws.com:5000'
+  //apiUrl: 'http://ec2-44-199-33-99.compute-1.amazonaws.com:5000'
+  apiUrl: 'http://localhost:5000'
 };
